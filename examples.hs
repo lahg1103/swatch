9e2dffd2fa2eca163b5c8d2d4585bf5984bd93c9
Leslie <hurtado@Leslies-MacBook-Pro-2.local>
2026-04-11 03:28:18 -0500

    CFG, AST, examples

--- examples.hs
+++ examples.hs
@@ -38,3 +38,42 @@
     <hex> -> <hexDigit><hexDigit><hexDigit> | <hexDigit><hexDigit><hexDigit><hexDigit><hexDigit><hexDigit>
     <hexDigit> -> [0-9] | [a-f] | [A-F]
 -}                 
+import Syntax
+
+--Example 1 - Palette creation
+{- 
+    create Greyscale [Foreground: #000, #ccc, Background: #eee]
+    add Greyscale #fff
+    css Greyscale
+-}
+-- Haskell
+example1 :: Program
+example1 = [
+    Create "Greyscale" [RoleEntry "Foreground" (Hex "000"), ColorEntry (Hex "ccc"), RoleEntry "Background" (Hex "eee")],
+    Add "Greyscale" (Hex "fff"),
+    CSS "Greyscale"
+            ]
+
+--Example 2 - Accessibility Aid
+{-
+    create Palette [Text: #eee, Paper: #aaa]
+    contrastCheck Palette Text Paper
+    shade #aaa
+-}
+-- Haskell
+example2 :: Program
+example2 = [
+    Create "Palette" [RoleEntry "Text" (Hex "eee"), RoleEntry "Paper" (Hex "#aaa")],
+    ContrastCheck (RolePair "Palette" "Text" "Paper"),
+    Transform Shade (Left (Hex "#aaa"))
+            ]
+
+main :: IO ()
+main = do
+    putStrLn "--- Swatch Demo ---"
+
+    putStrLn "\nExample 1 - Palette Creation:"
+    mapM_ print example1
+    
+    putStrLn "\nExample 2 - Accessibility Aid:"
+    mapM_ print example2
--- syntax.hs
+++ syntax.hs
@@ -7,11 +7,11 @@
     <Program> -> <statements>
     <statements> -> <statement> | <statement> <statements>
     <statement> ->
-                    "create palette" <name> "["<entries>"]"
+                    "create " <name> "["<entries>"]"
                   | "add" <name> <color>
                   | "remove" <name> <color> | "remove" <name>
                   | "update" <name> <color>
-                  | "convert" <mode> <mode> <color> <color>
+                  | "convert" <mode> <mode> <color> | "convert" <mode> <mode> <name>
 
                   | <transform> <target>
 
@@ -37,4 +37,82 @@
 
     <hex> -> <hexDigit><hexDigit><hexDigit> | <hexDigit><hexDigit><hexDigit><hexDigit><hexDigit><hexDigit>
     <hexDigit> -> [0-9] | [a-f] | [A-F]
--}                 
+-}
+module Syntax where
+import Data.List (intercalate)
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+
+type Program = [Statement]
+data Statement = Create Name [Entry]
+                | Add Name Color
+                | Remove Name (Maybe Color)
+                | Update Name Color
+                | Convert Mode Mode (Either Color Name)
+                | Assign Name Role Color
+                | Transform Transform (Either Color Name)
+                | ContrastCheck ContrastTarget
+                | CSS Name
+                | Print Name
+
+data Entry = ColorEntry Color | RoleEntry Role Color
+data Color = Hex String | RGB Value Value Value | HSL Value Value Value
+
+data Transform = Shade | Tint | Complementary | Tertiary | Analogous | Triadic
+data Target = ColorTarget Color | NameTarget Name
+
+data ContrastTarget = ColorPair Color Color
+                    | RolePair Name Role Role
+
+data Mode = HexMode | RGBMode | HSLMode
+
+instance Show Statement where
+    show (Create n entries) = "create " ++ n ++ " [" ++ intercalate ", " (map show entries) ++ "]"
+    show (Add n c) = "add " ++ n ++ " " ++ show c
+    show (Remove n maybeC) = "remove " ++ n ++ maybe ( "" ) (\c -> " " ++ show c ) maybeC
+    show (Update n c) = "update " ++ n ++ " " ++ show c
+    show (Convert m1 m2 t) = "convert " ++ show m1 ++ " " ++ show m2 ++ " " ++ (either show id t)
+    show (Transform t target) = show t ++ " " ++ showTarget target
+        where
+            showTarget (Left color) = show color
+            showTarget (Right name) = name
+    show (Assign n r c) = "assign " ++ n ++ " " ++ r ++ " " ++ show c
+    show (ContrastCheck ct) = "contrastCheck " ++ show ct
+    show (CSS n) = "css " ++ n
+    show (Print n) = "print " ++ n
+
+instance Show Color where
+    show (Hex s) = "#" ++ filter (/= '#') s
+    show (RGB r g b) = "rgb(" ++ show r ++ "," ++ show g ++ "," ++ show b ++ ")"
+    show (HSL h s l) = "hsl(" ++ show h ++ "," ++ show s ++ "," ++ show l ++ ")"
+
+instance Show Mode where
+    show HexMode = "hex"
+    show RGBMode = "rgb"
+    show HSLMode = "hsl"
+
+instance Show Transform where
+    show Shade = "shade"
+    show Tint = "tint"
+    show Complementary = "complementary"
+    show Tertiary = "tertiary"
+    show Analogous = "analogous"
+    show Triadic = "triadic"
+
+instance Show Target where
+    show (ColorTarget c) = show c
+    show (NameTarget n) = n
+
+instance Show ContrastTarget where
+    show (ColorPair c1 c2) = show c1 ++ " " ++ show c2
+    show (RolePair n r1 r2) = n ++ " " ++ r1 ++ " " ++ r2
+
+instance Show Entry where
+    show (ColorEntry c) = show c
+    show (RoleEntry r c) = show r ++ ": " ++ show c
+
+--types
+type Name = String
+type Role = String
+type Value = Int
